de6971c8a7d1ddaf0546bcfdb15ee6002c6dfc12
SergioNodriza <sergio.gomez@nodrizatech.com>
2020-11-03 11:17:36 +0100

    26.09

--- api/src/Api/Listener/JsonExceptionResponseTransformerListener.php
+++ api/src/Api/Listener/JsonExceptionResponseTransformerListener.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Api\listener;
+namespace App\Api\Listener;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
