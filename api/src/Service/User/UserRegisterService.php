254614d74649bf143867dcf87ad5b25fad3d1c57
SergioNodriza <sergio.gomez@nodrizatech.com>
2020-11-05 09:51:00 +0100

    38.08

--- api/src/Service/User/UserRegisterService.php
+++ api/src/Service/User/UserRegisterService.php
@@ -6,26 +6,27 @@ namespace App\Service\User;
 
 use App\Entity\User;
 use App\Exceptions\User\UserAlreadyExistsException;
+use App\Messenger\Message\UserRegisteredMessage;
+use App\Messenger\RoutingKey;
 use App\Repository\UserRepository;
 use App\Service\Password\EncoderService;
 use App\Service\Request\RequestService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class UserRegisterService
 {
     private UserRepository $userRepository;
     private EncoderService $encoderService;
-    /**
-     * @var EntityManagerInterface
-     */
-    private EntityManagerInterface $em;
+    private MessageBusInterface $messageBus;
 
-    public function __construct(UserRepository $userRepository, EncoderService $encoderService, EntityManagerInterface $em)
+    public function __construct(UserRepository $userRepository, EncoderService $encoderService, MessageBusInterface $messageBus)
     {
         $this->userRepository = $userRepository;
         $this->encoderService = $encoderService;
-        $this->em = $em;
+        $this->messageBus = $messageBus;
     }
 
     public function create(Request $request): User
@@ -39,14 +40,16 @@ class UserRegisterService
 
         $user->setPassword($this->encoderService->generateEncodedPassword($user, $password));
 
-        $this->em->persist($user);
-        $this->em->flush();
-
         try {
             $this->userRepository->save($user);
         } catch (\Exception $exception) {
-            //throw UserAlreadyExistsException::fromEmail($email);
+            throw UserAlreadyExistsException::fromEmail($email);
         }
+
+        $this->messageBus->dispatch(
+            new UserRegisteredMessage($user->getId(), $user->getName(), $user->getEmail(), $user->getToken()),
+            [new AmqpStamp(RoutingKey::USER_QUEUE)]
+        );
 
         return $user;
     }
