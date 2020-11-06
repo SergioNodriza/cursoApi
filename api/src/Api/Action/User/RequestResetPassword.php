45f20639202c3542c49f77e048c97b0a8b6d775f
SergioNodriza <sergio.gomez@nodrizatech.com>
2020-11-06 14:10:43 +0100

    53.04

---
+++ api/src/Api/Action/User/RequestResetPassword.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Api\Action\User;
+
+use App\Service\User\RequestResetPasswordService;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestResetPassword
+{
+    private RequestResetPasswordService $requestResetPassword;
+
+    public function __construct(RequestResetPasswordService $requestResetPassword)
+    {
+
+        $this->requestResetPassword = $requestResetPassword;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $this->requestResetPassword->send($request);
+
+        return new JsonResponse(['message' => 'Request reset password email sent']);
+    }
+}
