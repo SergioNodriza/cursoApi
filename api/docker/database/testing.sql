bdd45ac5b76513ebd182992eda79a500320caf8c
SergioNodriza <sergio.gomez@nodrizatech.com>
2020-11-09 10:55:57 +0100

    67.05

--- api/docker/database/testing.sql
+++ api/docker/database/testing.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE IF NOT EXISTS symfony_deb_test;
-GRANT ALL PRIVILEGES ON ymfony_db_test.* TO 'root' IDENTIFIED BY 'root'
+CREATE DATABASE IF NOT EXISTS symfony_db_test;
+GRANT ALL PRIVILEGES ON symfony_db_test.* TO 'root' IDENTIFIED BY 'root';
