2df460fdb3b11ce83a06e5a078767a7eb5b94426
SergioNodriza <sergio.gomez@nodrizatech.com>
2020-11-13 10:50:16 +0100

    113.06

---
+++ api/src/Service/Group/SendRequestToUserService.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Group;
+
+class SendRequestToUserService
+{
+    public function __construct()
+    {
+
+    }
+
+    public function send(string $groupId, string $email, string $requestedId): void
+    {
+
+    }
+}
